ecbbdd58b6aef1d449148b8b26d000b3d54f4ead
srihitha-ravi <s.ravi@pvp.co.jp>
2024-09-20 14:29:18 +0900

    Working but out of position

--- app/ui/dashboard/sidenav.jsx
+++ app/ui/dashboard/sidenav.jsx
@@ -2,13 +2,26 @@
 'use client';
 import Link from 'next/link';
 import NavLinks from './nav-links';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleNav = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
+    
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="flex grow flex-col bg-white-14 rounded-md items-left bg-contain pr-10">
-        <NavLinks />
+      {/* Toggle Button */}
+      <button onClick={toggleNav} className="p-2 text-white">
+        {/* Icon for collapsing */}
+        <Bars3Icon className="h-6 w-6" />
+      </button>
+      <div className={`flex grow flex-col bg-white-14 rounded-md bg-contain items-left transition-all duration-300 ${isCollapsed ? 'pr-0' : 'pr-10'}`}>
+        <NavLinks isCollapsed={isCollapsed}/>
         
       </div>
     </div>
